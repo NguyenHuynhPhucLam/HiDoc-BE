19a89cf57bcb7ed2d0eac0252e1bd83be64f1053
NguyenHuynhPhucLam <21520316@gm.uit.edu.vn>
2024-04-01 23:13:57 +0700

    Update Environment: MVC model

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import homeController from '../controllers/homeController';
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-  router.get('/', (req, res) => {
-    return res.send('Hello world');
+  router.get('/', homeController.getHomePage);
+  router.get('/Lam', (req, res) => {
+    return res.send('Phuc Lam');
   });
   return app.use('/', router);
 };
