a6869d647f4e4f6a18d1069a9ff2db3fd116d502
NguyenHuynhPhucLam <21520316@gm.uit.edu.vn>
2024-05-27 19:16:36 +0700

    getDoctorById Api

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -95,7 +95,7 @@ let getDoctorDetailByIdService = (inputId) => {
         let data = await db.User.findOne({
           where: { id: inputId },
           attributes: {
-            exclude: ['password', 'image'],
+            exclude: ['password'],
           },
           include: [
             {
@@ -108,9 +108,14 @@ let getDoctorDetailByIdService = (inputId) => {
               attributes: ['valueEn', 'valueVi'],
             },
           ],
-          raw: true,
+          raw: false,
           nest: true,
         });
+
+        if (data && data.image) {
+          data.image = new Buffer(data.image, 'base64').toString('binary');
+        }
+        if (!data) data = {};
 
         resolve({
           errCode: 0,
