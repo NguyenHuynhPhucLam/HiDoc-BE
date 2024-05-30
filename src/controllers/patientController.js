b667aca6715da3d34978559c60e48ca2247a6e40
NguyenHuynhPhucLam <21520316@gm.uit.edu.vn>
2024-05-30 15:34:20 +0700

    Verify booking through email

--- src/controllers/patientController.js
+++ src/controllers/patientController.js
@@ -14,6 +14,21 @@ let postBookAppointment = async (req, res) => {
   }
 };
 
+let postVerifyBookAppointment = async (req, res) => {
+  try {
+    let data = req.body;
+    let info = await patientService.postVerifyBookAppointmentService(data);
+    return res.status(200).json(info);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: 'Error from server ...',
+    });
+  }
+};
+
 module.exports = {
   postBookAppointment: postBookAppointment,
+  postVerifyBookAppointment: postVerifyBookAppointment,
 };
--- src/services/patientService.js
+++ src/services/patientService.js
@@ -1,6 +1,12 @@
 import db from '../models';
 require('dotenv').config();
+import { v4 as uuidv4 } from 'uuid';
 import emailService from './emailService';
+
+let buildUrlEmail = (doctorId, token) => {
+  let result = `${process.env.URL_REACT}/verify-booking?token=${token}&doctorId=${doctorId}`;
+  return result;
+};
 
 let postBookAppointmentService = (data) => {
   return new Promise(async (resolve, reject) => {
@@ -17,13 +23,14 @@ let postBookAppointmentService = (data) => {
           errMessage: 'Missing parameter',
         });
       } else {
+        let token = uuidv4();
         await emailService.sendSimpleEmail({
           receiverEmail: data.email,
           patientName: data.fullName,
           time: data.timeString,
           doctorName: data.doctorName,
           language: data.language,
-          redirectLink: 'https://blogtruyenmoi.com/',
+          redirectLink: buildUrlEmail(data.doctorId, token),
         });
         // Upsert patient
         let user = await db.User.findOrCreate({
@@ -44,6 +51,7 @@ let postBookAppointmentService = (data) => {
               patientId: user[0].id,
               date: data.date,
               timeType: data.timeType,
+              token: token,
             },
           });
         }
@@ -58,6 +66,45 @@ let postBookAppointmentService = (data) => {
   });
 };
 
+let postVerifyBookAppointmentService = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!data.token || !data.doctorId) {
+        resolve({
+          errCode: 1,
+          errMessage: 'Missing parameter',
+        });
+      } else {
+        let appointment = await db.Booking.findOne({
+          where: {
+            doctorId: data.doctorId,
+            token: data.token,
+            statusId: 'S1',
+          },
+          raw: false,
+        });
+
+        if (appointment) {
+          appointment.statusId = 'S2';
+          await appointment.save();
+          resolve({
+            errCode: 0,
+            errMessage: 'The user has confirmed booking',
+          });
+        } else {
+          resolve({
+            errCode: 2,
+            errMessage: 'Appointment has been activated or does not exist',
+          });
+        }
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
 module.exports = {
   postBookAppointmentService: postBookAppointmentService,
+  postVerifyBookAppointmentService: postVerifyBookAppointmentService,
 };
