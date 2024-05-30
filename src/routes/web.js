1821b1ba782c10d587171997ba7d59da7e6063a4
NguyenHuynhPhucLam <21520316@gm.uit.edu.vn>
2024-05-30 08:13:27 +0700

    postBookAppointmentService

--- src/routes/web.js
+++ src/routes/web.js
@@ -2,6 +2,7 @@ import express from 'express';
 import homeController from '../controllers/homeController';
 import userController from '../controllers/userController';
 import doctorController from '../controllers/doctorController';
+import patientController from '../controllers/patientController';
 
 let router = express.Router();
 
@@ -43,6 +44,12 @@ let initWebRoutes = (app) => {
     '/api/get-profile-doctor-by-id',
     doctorController.getProfileDoctorById
   );
+
+  router.post(
+    '/api/patient-book-appointment',
+    patientController.postBookAppointment
+  );
+
   return app.use('/', router);
 };
 
